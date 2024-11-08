b93ab7c3727db0505ec4b461eb95d8738921e747
Steven Guamán <stevengf14@gmail.com>
2024-11-07 22:44:01 -0500

    Create solution.js

---
+++ 169. Majority Element/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    let candidate = nums[0];
+    let count = 0;
+
+    for (let num of nums) {
+        if (count == 0) {
+            candidate = num;
+        }
+        count += (num == candidate) ? 1 : -1;
+    }
+    return candidate;
+};
