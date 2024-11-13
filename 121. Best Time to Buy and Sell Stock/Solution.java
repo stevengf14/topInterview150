dbdc13dc2298c947cda06cbf1edb42f81c541004
Steven Guamán <stevengf14@gmail.com>
2024-11-12 22:22:53 -0500

    Create Solution.java

---
+++ 121. Best Time to Buy and Sell Stock/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int min = prices[0];
+        int max = 0;
+
+        for (int price : prices) {
+            if (price < min) {
+                min = price;
+            } else if (price - min > max) {
+                max = price - min;
+            }
+        }
+
+        return max;
+    }
+}
